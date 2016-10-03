0bf9525b945e5a041aadd22ec37f559c07b3ed07
Sai Tejaswini Natani <saitejaswininatani@Sais-MacBook-Pro.local>
2016-10-03 14:32:10 -0700

    Third

--- Program.cs
+++ Program.cs
@@ -10,6 +10,16 @@ namespace ConsoleApplication
             Console.WriteLine("You can even directly print numbers such as the one below...");
             Console.WriteLine(favoriteNum);
             Console.WriteLine("Hello World!");
+            Console.WriteLine("***************");
+            //Making use of tokens makes injection of variables into strings quite simple
+            Console.WriteLine("The {0} cow, jumped over the {1}, {2} times!", "brown", "Moon", 7);
+            //Results in "The brown cow, jumped over the Moon, 7 times!"
+            //We can also make use of string concatenation though, if preferred
+            string name = "David";
+            Console.WriteLine(10 + " Chickens attacked " + name);
+            Console.WriteLine("***************");
+            string interpol = $"The answer to 2 + 7 is {2+7}";
+            Console.WriteLine(interpol);
         }
     }
 }
