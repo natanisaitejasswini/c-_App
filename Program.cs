7555e8f5f6c2a7d97f1bf04d34ba004ed5c49d60
Sai Tejaswini Natani <saitejaswininatani@Sais-MacBook-Pro.local>
2016-10-03 14:25:24 -0700

    Sec

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
+            int favoriteNum = 42;
+            Console.WriteLine("You can even directly print numbers such as the one below...");
+            Console.WriteLine(favoriteNum);
             Console.WriteLine("Hello World!");
         }
     }
